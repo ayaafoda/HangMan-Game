cc65018bdf46373e44d90ece59e4a380228fe77c
aya <ayafoda69@gmail.com>
2024-08-11 08:52:34 +0300

    Add File

---
+++ js/index.js
@@ -0,0 +1,131 @@
+let alphabet = document.querySelector(".alphabet");
+let letters = "abcdefghijklmnopqrstuvwxyz";
+let lett = Array.from(letters);
+let man = document.querySelectorAll(".theMan .all");
+
+// the word
+let word = document.getElementsByClassName("theword")[0];
+console.log(Array.from(man));
+console.log(man);
+
+let myWords = ["boy", "girl", "snack"];
+
+let randomWord = Math.floor(Math.random() * myWords.length);
+let thecurrentWord = myWords[randomWord];
+let dashed = Array.from("_".repeat(myWords[randomWord].length));
+
+Array.from(thecurrentWord).forEach(() => {
+  let span = document.createElement("span");
+  span.innerHTML = "_";
+  word.appendChild(span);
+});
+console.log(Array.from(dashed));
+dashed.join(" ");
+let theTrueLwtter = [];
+// word.innerHTML = dashed;
+
+lett.forEach((letter) => {
+  let span = document.createElement("span");
+  span.className = "letter";
+  span.innerHTML = letter;
+  alphabet.appendChild(span);
+});
+
+let spans = Array.from(document.querySelectorAll(".theword span"));
+console.log(spans)
+let lives = 8;
+document.addEventListener("click", (e) => {
+  let statue = false;
+  if (e.target.className == "letter") {
+    e.target.style.pointerEvents = "none";
+    e.target.style.backgroundColor = "#eda4a9";
+    theTrueLwtter.unshift(e.target.innerHTML);
+    Array.from(thecurrentWord).forEach((letterr, index) => {
+      if (e.target.innerHTML == letterr) {
+        // lives--
+        statue = true;
+        console.log(statue);
+        // console.log(lives)
+        spans.forEach((spanletter, spanindex) => {
+          if (spanindex == index) {
+            spanletter.innerHTML = e.target.innerHTML;
+          }
+        });
+      }
+    });
+  }
+  if (statue !== true && e.target.className == "letter") {
+    lives--;
+    console.log(lives);
+  }
+  // spans.forEach((dash) => {
+  //   if (dash.innerHTML == "_") {
+  //     document.querySelector(".win").style.display = "block";
+
+  //   }
+  // })
+  switch (lives) {
+    case 7:
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 6:
+      man[0].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 5:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 4:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[2].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 3:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[2].style.display = "block";
+      man[3].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 2:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[2].style.display = "block";
+      man[3].style.display = "block";
+      man[4].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 1:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[2].style.display = "block";
+      man[3].style.display = "block";
+      man[4].style.display = "block";
+      man[5].style.display = "block";
+      man[7 - lives].style.display = "block";
+
+      break;
+    case 0:
+      man[0].style.display = "block";
+      man[1].style.display = "block";
+      man[2].style.display = "block";
+      man[3].style.display = "block";
+      man[4].style.display = "block";
+      man[5].style.display = "block";
+      man[6].style.display = "block";
+      man[7 - lives].style.display = "block";
+      document.querySelector(".popup")[0].style.display = "block"
+      break;
+  }
+});
+
+console.log(thecurrentWord);
